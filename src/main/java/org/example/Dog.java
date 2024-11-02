8673c4493e1f7c31846615d52ce4597b006e9476
QRWER <mum500678@gmail.com>
2024-11-02 20:20:46 +0400

    Прошел конфигурацию с помощью аннотаций

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -1,6 +1,14 @@
 package org.example;
 
-public class Dog implements Pet{
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component("dogBean")
+@Scope
+public class Dog implements Pet {
 
     public Dog() {
         System.out.println("Dog bean is created");
@@ -9,5 +17,15 @@ public class Dog implements Pet{
     @Override
     public void say(){
         System.out.println("Gow-Wow");
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Dog init method");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Dog destroy method");
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,19 +1,30 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
+    @Autowired
+    @Qualifier("catBean")
     private Pet pet;
+//    @Value("${person.surname}")
     private String surname;
+//    @Value("${person.age}")
     private int age;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("Person bean is created");
-        this.pet = pet;
     }
 
-    public Person(){
-        System.out.println("Person bean with setter is created");
-    }
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
 
+//    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
