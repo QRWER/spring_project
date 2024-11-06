282a282a7f182eede8fce62cb70658582bd0b63a
Skalei Ilya <mum500678@gmail.com>
2024-11-06 10:29:07 +0400

    Настройка Spring Container с помощью джава класса

---
+++ src/main/java/org/example/MyConfig.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+@PropertySource("classpath:myApp.properties")
+public class MyConfig {
+
+    @Bean
+    @Scope("singleton")
+    public Pet catBean(){
+        return new Cat();
+    }
+
+    @Bean
+    public Person personBean(){
+        return new Person(catBean());
+    }
+
+}
